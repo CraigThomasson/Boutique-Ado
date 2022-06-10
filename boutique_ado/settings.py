e585d961550e9a2bfdd68b0a154830788e30c549
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-06-10 18:25:19 +0000

    setup for horoku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-o9p)pcflp43+74anorzf_$u41^^1xrriey9hjif-v$u=t@6-xo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['craigs-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
